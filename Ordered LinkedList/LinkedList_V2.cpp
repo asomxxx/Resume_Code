a3480b88c2c798343cd09425dc8822d4f4077dbf
asomxxx <e.j.ashford@gmail.com>
2016-08-24 17:23:04 -0400

    Fixed Tab vs Space formatting

--- Ordered LinkedList/LinkedList_V2.cpp
+++ Ordered LinkedList/LinkedList_V2.cpp
@@ -4,90 +4,90 @@ using namespace std;
 
 class LinkedList
 {
-private:
-  struct Node
-  {
-    int number;
-    Node* next;
-  };
-  Node* head;
-
-public:
-  LinkedList()
-  {head = NULL;}
-
-
-  bool addNode(int num)
-  {
-    Node* tracker;
-    Node* previous;
-    Node* newNode = new Node;
-    newNode->number = num;
-
-    //Empty List
+	private:
+		struct Node
+		{
+			int number;
+			Node* next;
+		};
+		Node* head;
+	
+	public:
+	LinkedList()
+	{head = NULL;}
+	
+	bool addNode(int num)
+	{
+		Node* tracker;
+		Node* previous;
+		Node* newNode = new Node;
+		newNode->number = num;
+	
+		//Empty List
 		if(head == NULL)
 		{
 			head = newNode;
 			newNode->next = NULL;
 			return true;
 		}
-
-    //Non-Empty List
-    else
-    {
-    	tracker = head;
-    	previous = NULL;
-    	while(tracker-> next)
-    	{
-    		if(tracker->number > num)	//Put the number here
-    		{
-    			if(previous == NULL)	//if first node
-    			{
-    				newNode->next = head;
-    				head = newNode;
-    				return true;
-    			}
-    			else		//NEither first or last
-    			{
-    				previous->next = newNode;
-    				newNode->next = tracker;
-    				return true;
-    			}
-    		}
-    		else //Move to check next node
-    		{
-    		previous = tracker;
-    		tracker = tracker->next;
-    		}
-    	}
-    	//At end of list, add and make next = NULL ( nulptr if c++11 )
-    	tracker->next = newNode;
-    	newNode->next = NULL;
-    	return true;
-    }
-    //I dont see how you could get here but better safe than sorry?
-  	return false;
-  }
-
-  bool findNode(int num)
-  {
-    Node* tracker = head;
-    while(tracker->next)
-    {
-      if(tracker->number == num)
-      {
-        return true;
-      }
-      tracker = tracker->next;
-    }
-    //Get last number in list
-    if(tracker->number == num)
-    {
-      return true;
-    }
-    return false;
-  }
-
+		
+		//Non-Empty List
+		else
+		{
+			tracker = head;
+			previous = NULL;
+			while(tracker-> next)
+			{
+				if(tracker->number > num)	//Put the number here
+				{
+					if(previous == NULL)	//if first node
+					{
+						newNode->next = head;
+						head = newNode;
+						return true;
+					}
+					else		//NEither first or last
+					{
+						previous->next = newNode;
+						newNode->next = tracker;
+						return true;
+					}
+				}
+				else //Move to check next node
+				{
+					previous = tracker;
+					tracker = tracker->next;
+				}
+			}
+			//At end of list, add and make next = NULL ( nulptr if c++11 )
+			tracker->next = newNode;
+			newNode->next = NULL;
+			return true;
+		}
+		return false;
+	}
+	
+	bool findNode(int num)
+	{
+		Node* tracker = head;
+		while(tracker->next)
+		{
+			//Check each node for number and return true if found
+			if(tracker->number == num)
+			{
+				return true;
+			}
+		tracker = tracker->next;
+		}
+		//Check last number in list
+		if(tracker->number == num)
+		{
+			return true;
+		}
+		//Not found return false
+		return false;
+	}
+	
 	void displayList()0
 	{
 		if(head == NULL)
@@ -96,7 +96,7 @@ public:
 		{
 			int count = 0;
 			Node* tracker = head;
-      cout<<"-----------------------------------------"<<endl;
+			cout<<"-----------------------------------------"<<endl;
 			while(tracker->next)
 			{
 				cout<<"Node "<<count<<" | Data "<<tracker->number<<endl;
@@ -105,7 +105,7 @@ public:
 			}
 			// For the last node in the list
 			cout<<"Node "<<count<<" | Data "<<tracker->number<<endl;
-      cout<<"-----------------------------------------"<<endl;
+			cout<<"-----------------------------------------"<<endl;
 		}
 	}
 
@@ -115,17 +115,17 @@ public:
 int main()
 {
 	LinkedList list;
-  int checknum;
-  for(int i = 0 ; i<100 ;i++)
-  {
-    list.addNode(i);
-  }
-  list.displayList();
-  cout<<"Enter a number to check the list for > ";
-  cin>>checknum;
-  if(list.findNode(checknum))
-    cout<<checknum<< " was found in the list\n";
-  else
-    cout<<checknum<<" was not found in the list\n";
-  return 0;
+	int checknum;
+	for(int i = 0 ; i<100 ;i++)
+	{
+		list.addNode(i);
+	}
+	list.displayList();
+	cout<<"Enter a number to check the list for > ";
+	cin>>checknum;
+	if(list.findNode(checknum))
+		cout<<checknum<< " was found in the list\n";
+	else
+		cout<<checknum<<" was not found in the list\n";
+	return 0;
 }
